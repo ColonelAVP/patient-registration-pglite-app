b873c456467eca07482dea4a5805beda6a8666f2
Colonel-AVP <atherv.patil@proemsports.com>
2025-05-04 13:27:49 +0530

    chore: initial Vite + Tailwind CSS setup

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './styles/index.css';
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className="text-2xl text-blue-600 font-bold">Tailwind is working ✅</div>
+    </>
+  )
+}
+
+export default App
